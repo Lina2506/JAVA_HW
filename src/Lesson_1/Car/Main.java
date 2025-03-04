f4a9a025c5c5f10e56e99d3cef2a3bdbd63b0780
Lina2506 <allarenotme.lina@gmail.com>
2025-03-04 16:56:18 +0200

    change Car

--- src/Lesson_1/Car/Main.java
+++ src/Lesson_1/Car/Main.java
@@ -6,10 +6,11 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Car> cars = new ArrayList<>();
 
-        cars.add(new Car("BMW",34.2,3,true));
-        cars.add(new Car("Toyota",34.2,1,false));
-        cars.add(new Car("Porsche",2.5,2,true));
-        cars.add(new Car("Volvo",34.2,4,false));
+        cars.add(new Car("BMW",100.0,5,true));
+        cars.add(new Car("Toyota",60.2,1,false));
+        cars.add(new Car("Porsche",80.5,2,true));
+        cars.add(new Car("Volvo",45.2,4,false));
+        cars.add(new Car("Honda",34.2,3,false));
 
         for (Car car : cars) {
             System.out.println(car);
