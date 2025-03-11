44c77ea002d6aa5c533e6d5e1c9f38d4be98af1a
Lina2506 <allarenotme.lina@gmail.com>
2025-03-11 12:25:16 +0200

    sorting done

--- src/Lesson_4/Main.java
+++ src/Lesson_4/Main.java
@@ -2,7 +2,8 @@ package Lesson_4;
 
 import Lesson_4.User.User;
 
-import java.util.ArrayList;
+import java.util.*;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -14,6 +15,22 @@ public class Main {
         users.add(new User(5,"Max", 75));
 
         //Сортування за віком:
+        users.sort(Comparator.comparingInt(User::getAge));
+        System.out.println("Сортування за віком в порядку зростання: " + users);
+        users.sort(Comparator.comparingInt(User::getAge).reversed());
+        System.out.println("Сортування за віком в порядку спадання: " + users);
 
+        //Сортування за кількістю знаків в імені:
+        users.sort(Comparator.comparingInt(User->User.getName().length()));
+        System.out.println("Cортування за довжиною імені за зростанням: " + users);
+        users.sort(Comparator.comparingInt(User-> -(User.getName().length())));  ;
+        System.out.println("Cортування за довжиною імені за спаданням: " + users);
+
+
+        //ArrayList на 15 елементів___________________________________________//
+        ArrayList<String> words = new ArrayList<>(Arrays.asList("jeans","egg","car","ocean","dog","bus","gap","lama","icon","apple",
+                "kiwi","hi","music","nike","fly"));
+        Collections.sort(words);
+        System.out.println(words);
     }
 }
