2a31a9d617d5694e32b01133d35abe38966be4b8
Lina2506 <allarenotme.lina@gmail.com>
2025-03-11 09:50:59 +0200

    create ListUsers

---
+++ src/Lesson_4/Main.java
@@ -0,0 +1,19 @@
+package Lesson_4;
+
+import Lesson_4.User.User;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<User> users = new ArrayList<>();
+        users.add(new User(1,"Vasia", 30));
+        users.add(new User(2,"Olya", 15));
+        users.add(new User(3,"Oz", 10));
+        users.add(new User(4,"Valentina", 50));
+        users.add(new User(5,"Max", 75));
+
+        //Сортування за віком:
+
+    }
+}
